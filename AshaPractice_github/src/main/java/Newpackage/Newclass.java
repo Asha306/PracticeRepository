4822d3fcfece56902550f70e59d3bfdf6e848cbd
USER <USER@DESKTOP-7SM6G84>
2024-11-07 17:58:06 +0530

    new project for practicing

---
+++ AshaPractice_github/src/main/java/Newpackage/Newclass.java
@@ -0,0 +1,12 @@
+package Newpackage;
+
+public class Newclass {
+public static void main(String[] args) {
+	System.out.println("hii");
+	System.out.println("hello");
+	System.out.println("h r u");
+	System.out.println("wt r u doing");
+    System.out.println("had launch");
+
+}
+}
