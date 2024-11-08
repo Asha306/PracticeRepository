4c6cbc06884e7f2d378fdd3175b0c43f81b68b9c
USER <USER@DESKTOP-7SM6G84>
2024-11-08 07:13:11 +0530

    changes are done in veeragaru work space

--- AshaPractice_github/src/main/java/Newpackage/Newclass.java
+++ AshaPractice_github/src/main/java/Newpackage/Newclass.java
@@ -11,6 +11,7 @@ public static void main(String[] args) {
     System.out.println("how is your days");
 
     System.out.println("how is ur day");
+    System.out.println("exam ela rasavuu");
 
 
 }
